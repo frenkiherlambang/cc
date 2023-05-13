79ea6aea277f123dbcf58c1073b1f05c6aa33a36
Frenki Herlambang Prasetyo <frenkiherlambang@gmail.com>
2023-05-13 12:31:11 +0700

    Take Home Test Coding Collective

---
+++ src/ewallet/routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('deposit', function (Request $request) {
+
+    // check if bearer token is valid
+    if ($request->bearerToken() !== base64_encode('Frenki Herlambang Prasetyo')) {
+        return [
+            'status' => 'error',
+            'message' => 'Unauthorized',
+        ];
+    }
+
+    return [
+        'order_id' => $request->input('order_id'),
+        'amount' => $request->input('amount'),
+        'status' => 1,
+    ];
+    $orderId = $request->input('order_id');
+    $amount = $request->input('amount');
+    $timestamp = $request->input('timestamp');
+
+    // Process the deposit request
+    // ...
+
+    // Return a response
+    return [
+        'status' => 'success',
+        'message' => 'Deposit successful',
+    ];
+});
